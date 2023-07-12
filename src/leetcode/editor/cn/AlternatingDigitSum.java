455ac07dfd6d48082f02a8b3527c8b209f42d8b6
lazwa <1065423410@qq.com>
2023-07-12 09:12:11 +0800

    Submit Solution - 2023-07-12 09:12:11

---
+++ src/leetcode/editor/cn/AlternatingDigitSum.java
@@ -0,0 +1,73 @@
+/**
+给你一个正整数 n 。n 中的每一位数字都会按下述规则分配一个符号： 
+
+ 
+ 最高有效位 上的数字分配到 正 号。 
+ 剩余每位上数字的符号都与其相邻数字相反。 
+ 
+
+ 返回所有数字及其对应符号的和。 
+
+ 
+
+ 示例 1： 
+
+ 
+输入：n = 521
+输出：4
+解释：(+5) + (-2) + (+1) = 4 
+
+ 示例 2： 
+
+ 
+输入：n = 111
+输出：1
+解释：(+1) + (-1) + (+1) = 1
+ 
+
+ 示例 3： 
+
+ 
+输入：n = 886996
+输出：0
+解释：(+8) + (-8) + (+6) + (-9) + (+9) + (-6) = 0
+ 
+
+ 
+
+ 提示： 
+
+ 
+ 1 <= n <= 10⁹ 
+ 
+
+ 
+
+ Related Topics 数学 👍 20 👎 0
+
+*/
+
+package leetcode.editor.cn;
+
+/**
+ * @author wangzhiheng
+ */
+public class AlternatingDigitSum {
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int alternateDigitSum(int n) {
+            int res = 0;
+            while (n > 0) {
+                res = n % 10 - res;
+                n /= 10;
+            }
+
+            return res;
+        }
+    }
+    //leetcode submit region end(Prohibit modification and deletion)
+
+    public static void main(String[] args) {
+        Solution solution = new AlternatingDigitSum().new Solution();
+    }
+}
